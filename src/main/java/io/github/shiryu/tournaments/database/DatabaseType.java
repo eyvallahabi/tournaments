91515be70cd74d633b178bb4132e27bdf95113f5
efe <shiryusword@gmail.com>
2023-03-20 18:14:13 +0300

    recode start

--- src/main/java/io/github/shiryu/tournaments/database/DatabaseType.java
+++
@@ -1,7 +0,0 @@
-package io.github.shiryu.tournaments.database;
-
-public enum DatabaseType {
-
-    MYSQL,
-    SQLITE
-}
--- src/main/java/io/github/shiryu/tournaments/listener/bukkit/BukkitListener.java
+++
@@ -1,7 +0,0 @@
-package io.github.shiryu.tournaments.listener.bukkit;
-
-import io.github.shiryu.tournaments.listener.Listener;
-import org.bukkit.event.Event;
-
-public interface BukkitListener<E extends Event> extends Listener<E> {
-}
